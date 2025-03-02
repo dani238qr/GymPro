8cabbd508707f470e5c6bf1f63d114d93760be0b
Daniel P. <147325072+dani238qr@users.noreply.github.com>
2025-03-02 07:59:04 -0800

    Add files via upload

---
+++ Subscription.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymPro
+{
+    class Subscription
+    {
+        public string Type { get; set; }
+        public string Options { get; set; }
+        public decimal Price { get; set; }
+        public int DurationDays { get; set; }
+
+        public Subscription(string type, string options, decimal price, int durationDays)
+        {
+            Type = type;
+            Options = options;
+            Price = price;
+            DurationDays = durationDays;
+        }
+    }
+}
